e041c5806919783714dd00756cba1038e9194e68
ankitjain0748 <ankitjain0748@gmail.com>
2024-10-01 06:48:15 +0530

    manage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./Admin/Login/login";
+import Login from "./Admin/Login/Login";
+import Index from "./Admin/Page/Index";
 function App() {
 
   return (
     <div id="body-pd" className="App">
         <Router>
           <Routes>
-            <Route path="/" element={<Login />} />
+          <Route path="/" element={<Index />} />
+
+            <Route path="/login" element={<Login />} />
 
           </Routes>
         </Router>
