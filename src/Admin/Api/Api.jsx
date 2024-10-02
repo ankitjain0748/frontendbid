ecfab5e8c9028f9249086dbc84db9635ef94eee2
ankitjain0748 <ankitjain0748@gmail.com>
2024-10-02 22:24:23 +0530

    first commit

--- src/Admin/Api/Api.jsx
+++ src/Admin/Api/Api.jsx
@@ -1,30 +1,32 @@
 import axios from 'axios';
-const APP_URL = process.env.REACT_APP_URL
-console.log(APP_URL)
+
+// The base URL of your backend
+const APP_URL = process.env.REACT_APP_URL || 'http://localhost:5000';
+
+// Function to get the token from local storage
 function getToken() {
-  const data = localStorage && localStorage.getItem('token');
-  return data; 
+  return localStorage.getItem('token');
 }
 
+// Create an Axios instance
 let Api = axios.create({
   baseURL: APP_URL,
   headers: {
     'Accept': 'application/json',
-    "mode" :"no-cors",
-    'Authorization': `Bearer ${getToken()}`
   }
 });
 
+// Set up an interceptor to add the Authorization token to each request
 Api.interceptors.request.use(
   async (config) => {
-      const token = getToken();
-      if (token !== null) {
-          config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config; 
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
   },
   (error) => {
-      return Promise.reject(error);
+    return Promise.reject(error);
   }
 );
 
--- src/Admin/Login/Login.jsx
+++ src/Admin/Login/Login.jsx
@@ -69,7 +69,7 @@ const Login = () => {
                 name="phone"
                 value={Regs?.phone}
                 onChange={handleInputs}
-                type="number"
+                type="text"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
